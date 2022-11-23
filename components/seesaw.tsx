7cc8a8123cc1a38d0091d385fe78a877d111a98a
aoue-hyg <aoue.hyg@gmail.com>
2022-11-23 10:07:59 +0800

    seesaw

---
+++ components/seesaw.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/seesaw.module.scss'
+const Seesaw = () => {
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.stick}>
+        <div className={styles.ball} />
+      </div>
+    </div>
+  )
+}
+
+export default Seesaw
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>pure-css-element</title>
+        <meta name="description" content="pure-css-element" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
